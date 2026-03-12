GNOME/gtk

commit a78873ae56497927d84c14c9ecbcbaacd5a2db8c

Plug GTask leaks
Signed-off-by: Sergey Bugaev <bugaevc@gmail.com>
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9366>

gtk/gtkprintdialog.c
@@ -1218,6 +1218,7 @@ print_called (GObject      *source,
       ptd->has_returned = TRUE;
       g_object_add_weak_pointer (G_OBJECT (ptd->stream), (gpointer *)&ptd->stream);
       g_task_return_pointer (task, ptd->stream, g_object_unref);
+      g_object_unref (task);
     }
 }
 

gdk/filetransferportal.c
@@ -267,6 +267,7 @@ retrieve_files_done (GObject      *object,
   g_variant_unref (ret);
 
   g_task_return_boolean (task, TRUE);
+  g_object_unref (task);
 }
 
 void

gdk/gdkclipboard.c
@@ -729,6 +729,7 @@ gdk_clipboard_read_value_got_stream (GObject      *source,
   if (stream == NULL)
     {
       g_task_return_error (task, error);
+      g_object_unref (task);
       return;
     }
 

gdk/gdkdrop.c
@@ -761,6 +761,7 @@ gdk_drop_read_value_got_stream (GObject      *source,
   if (stream == NULL)
     {
       g_task_return_error (task, error);
+      g_object_unref (task);
       return;
     }
 

gtk/print/backends/gtkcupssecretsutils.c
@@ -121,6 +121,7 @@ get_secret_cb (GObject      *source_object,
   if (output == NULL)
     {
       g_task_return_error (task, error);
+      g_object_unref (task);
       return;
     }
 
@@ -131,6 +132,7 @@ get_secret_cb (GObject      *source_object,
       GTK_DEBUG (PRINTING, "Failed to lookup attributes.");
       g_variant_unref (output);
       g_task_return_pointer (task, NULL, NULL);
+      g_object_unref (task);
       return;
     }
 
@@ -223,6 +225,7 @@ get_secret_cb (GObject      *source_object,
     }
 
   g_task_return_pointer (task, auth_info, NULL);
+  g_object_unref (task);
   return;
 
 fail:
@@ -237,6 +240,7 @@ fail:
     }
   g_free (auth_info);
   g_task_return_pointer (task, NULL, NULL);
+  g_object_unref (task);
 }
 
 static void
@@ -257,6 +261,7 @@ create_item_cb (GObject      *source_object,
   if (output == NULL)
     {
       g_task_return_error (task, error);
+      g_object_unref (task);
       return;
     }
 
@@ -265,6 +270,7 @@ create_item_cb (GObject      *source_object,
     {
       GTK_DEBUG (PRINTING, "Successfully stored auth info.");
       g_task_return_pointer (task, NULL, NULL);
+      g_object_unref (task);
       return;
     }
   g_variant_unref (output);
@@ -318,6 +324,7 @@ do_store_auth_info (GTask *task)
     {
       GTK_DEBUG (PRINTING, "Failed to create attributes.");
       g_task_return_pointer (task, NULL, NULL);
+      g_object_unref (task);
       return;
     }
 
@@ -325,6 +332,7 @@ do_store_auth_info (GTask *task)
     {
       GTK_DEBUG (PRINTING, "No secret to store.");
       g_task_return_pointer (task, NULL, NULL);
+      g_object_unref (task);
       return;
     }
 
@@ -389,6 +397,7 @@ prompt_completed_cb (GDBusConnection *connection,
     {
       GTK_DEBUG (PRINTING, "Invalid prompt signal.");
       g_task_return_pointer (task, NULL, NULL);
+      g_object_unref (task);
       return;
     }
 
@@ -399,6 +408,7 @@ prompt_completed_cb (GDBusConnection *connection,
     {
       GTK_DEBUG (PRINTING, "Collection unlock dismissed.");
       g_task_return_pointer (task, NULL, NULL);
+      g_object_unref (task);
       return;
     }
 
@@ -444,6 +454,7 @@ prompt_cb (GObject      *source_object,
   if (output == NULL)
     {
       g_task_return_error (task, error);
+      g_object_unref (task);
       return;
     }
 
@@ -483,6 +494,7 @@ unlock_collection_cb (GObject      *source_object,
   if (output == NULL)
     {
       g_task_return_error (task, error);
+      g_object_unref (task);
       return;
     }
 
@@ -552,6 +564,7 @@ unlock_read_alias_cb (GObject      *source_object,
   if (output == NULL)
     {
       g_task_return_error (task, error);
+      g_object_unref (task);
       return;
     }
 
@@ -562,6 +575,7 @@ unlock_read_alias_cb (GObject      *source_object,
     {
       GTK_DEBUG (PRINTING, "Invalid ReadAlias response.");
       g_task_return_pointer (task, NULL, NULL);
+      g_object_unref (task);
       return;
     }
 
@@ -607,6 +621,7 @@ item_proxy_cb (GObject      *source_object,
   if (item_proxy == NULL)
     {
       g_task_return_error (task, error);
+      g_object_unref (task);
       return;
     }
 
@@ -618,6 +633,7 @@ item_proxy_cb (GObject      *source_object,
     {
       GTK_DEBUG (PRINTING, "Failed to look up \"Locked\" property on item.");
       g_task_return_pointer (task, NULL, NULL);
+      g_object_unref (task);
       return;
     }
 
@@ -687,6 +703,7 @@ search_items_cb (GObject      *source_object,
   if (output == NULL)
     {
       g_task_return_error (task, error);
+      g_object_unref (task);
       return;
     }
 
@@ -733,6 +750,7 @@ search_items_cb (GObject      *source_object,
     {
       GTK_DEBUG (PRINTING, "No match found in secrets service.");
       g_task_return_pointer (task, NULL, NULL);
+      g_object_unref (task);
       return;
     }
 }
@@ -757,6 +775,7 @@ open_session_cb (GObject      *source_object,
   if (output == NULL)
     {
       g_task_return_error (task, error);
+      g_object_unref (task);
       return;
     }
 
@@ -767,6 +786,7 @@ open_session_cb (GObject      *source_object,
       GTK_DEBUG (PRINTING, "Invalid session path response.");
       g_variant_unref (output);
       g_task_return_pointer (task, NULL, NULL);
+      g_object_unref (task);
       return;
     }
 
@@ -778,6 +798,7 @@ open_session_cb (GObject      *source_object,
       g_variant_unref (session_variant);
       g_variant_unref (output);
       g_task_return_pointer (task, NULL, NULL);
+      g_object_unref (task);
       return;
     }
 
@@ -796,6 +817,7 @@ open_session_cb (GObject      *source_object,
             {
               GTK_DEBUG (PRINTING, "Failed to create attributes.");
               g_task_return_pointer (task, NULL, NULL);
+              g_object_unref (task);
               return;
             }
 
@@ -852,6 +874,7 @@ get_connection_cb (GObject      *source_object,
   if (task_data->dbus_connection == NULL)
     {
       g_task_return_error (task, error);
+      g_object_unref (task);
       return;
     }
 

ElectronSpark/v6-gtk

commit cdeff2d00a228a937ef29e4b26ea2c9f2dd68fac

printdialog: Keep GTask alive for portal repsonse
We might return/unref the GTask before the portal response.
The portal response handler uses the GTask data so it must be kept alive.

Fixes: a78873ae56 ("Plug GTask leaks")

gtk/gtkprintdialog.c
@@ -1160,8 +1160,6 @@ print_response (GDBusConnection *connection,
           break;
         }
     }
-
-  g_object_unref (task);
 }
 
 static void
@@ -1204,7 +1202,8 @@ print_called (GObject      *source,
                                             NULL,
                                             G_DBUS_SIGNAL_FLAGS_NO_MATCH_RULE,
                                             print_response,
-                                            task, NULL);
+                                            g_object_ref (task),
+                                            g_object_unref);
 
     }
 
@@ -1218,8 +1217,8 @@ print_called (GObject      *source,
       ptd->has_returned = TRUE;
       g_object_add_weak_pointer (G_OBJECT (ptd->stream), (gpointer *)&ptd->stream);
       g_task_return_pointer (task, ptd->stream, g_object_unref);
-      g_object_unref (task);
     }
+  g_object_unref (task);
 }
 
 static void
@@ -1255,7 +1254,8 @@ print_window_handle_exported (GtkWindow  *window,
                                         NULL,
                                         G_DBUS_SIGNAL_FLAGS_NO_MATCH_RULE,
                                         print_response,
-                                        task, NULL);
+                                        g_object_ref (task),
+                                        g_object_unref);
 
   fd_list = g_unix_fd_list_new ();
   idx = g_unix_fd_list_append (fd_list, ptd->fds[0], NULL);